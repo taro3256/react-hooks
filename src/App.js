f94cfe352b4f46fab03b26ae0274372ca7384980
amaisuna <taro30143014@gmail.com>
2019-08-16 18:57:59 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = props => {
 
   const [state, setState] = useState(props)
   const { name, price } = state
-  
+
+  // 最初と変更がある度に出力
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate')
+  })
+
+  // 最初だけ出力
+  useEffect(() => {
+    console.log('This is like componentDidMount')
+  }, [])
+
+  // nameが変更された時だけ出力
+  useEffect(() => {
+    console.log('This callback is for name only')
+  }, [name])
+
   return (
     <>
-      <p>現在の{ name }は、{ price }円です</p>
-      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
-      <button onClick={() => setState(props) }>Reset</button>
-      <input value={ name } onChange={e => setState({...state, name: e.target.value})}/>
+      <p>現在の{name}は、{price}円です</p>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={name} onChange={e => setState({ ...state, name: e.target.value })} />
     </>
   )
 }
